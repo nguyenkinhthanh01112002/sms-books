2575223182624192d9f55621781d026c9ae093cb
nguyenkinhthanh01112002 <nguyenkinhthanh11@gmail.com>
2024-09-12 02:24:27 +0700

    hoan thanh cau hinh base du an

---
+++ BookManagement/BookManagement.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookManagement.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
